11fdcb021954be9a03773591ab98ff75f9e91189
porovoz <falkon1@list.ru>
2024-04-24 12:57:14 +0300

    scheduling added

--- src/main/java/com/bestapp/MonitoringSystem/MonitoringSystemApplication.java
+++ src/main/java/com/bestapp/MonitoringSystem/MonitoringSystemApplication.java
@@ -2,8 +2,10 @@ package com.bestapp.MonitoringSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MonitoringSystemApplication {
 
 	public static void main(String[] args) {
